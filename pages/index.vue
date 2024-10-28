fde61760bb644ed4a142cb1b12854e53e884a798
anas-omar-abdullah <ansomar2525@gmail.com>
2024-10-28 23:53:05 +0300

    start-project

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <Button>Click me</Button>
+         hello
+    </div>
+</template>
+
+<script setup>
+    const loading = ref(false)
+</script>
+
+<style lang="scss" scoped>
+
+</style>
